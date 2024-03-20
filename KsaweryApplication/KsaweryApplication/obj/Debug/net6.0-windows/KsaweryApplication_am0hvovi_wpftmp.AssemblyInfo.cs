5680be711cf1baa6114097d2c0efa4eae00e8c73
DevDeVice <sejkop@gmail.com>
2024-03-20 13:28:43 +0100

    Zmienic opis to ANG jak beda dane

--- KsaweryApplication/KsaweryApplication/obj/Debug/net6.0-windows/KsaweryApplication_am0hvovi_wpftmp.AssemblyInfo.cs
+++ KsaweryApplication/KsaweryApplication/obj/Debug/net6.0-windows/KsaweryApplication_am0hvovi_wpftmp.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("KsaweryAPP")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("KsaweryApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("KsaweryAPP")]
-[assembly: System.Reflection.AssemblyTitleAttribute("KsaweryAPP")]
+[assembly: System.Reflection.AssemblyProductAttribute("KsaweryApplication")]
+[assembly: System.Reflection.AssemblyTitleAttribute("KsaweryApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
